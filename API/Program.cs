61f1feeef17bd96eda759d86edf8f0ab64e00238
Jesse <Jesse@DESKTOP-GMP92T4>
2025-03-18 11:32:46 -0400

    Implimented repository patterns and added getby http methods (3)

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
@@ -23,5 +26,19 @@ var app = builder.Build();
 
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var service = scope.ServiceProvider;
+    var context = service.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+} 
+catch (Exception ex) 
+{
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
